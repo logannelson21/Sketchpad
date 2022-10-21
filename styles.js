e8efa13aced2f8244a3000934ff84543a58c8089
Logan <logan.nelson012@gmail.com>
2022-10-21 15:21:26 -0700

    Currently working through the grid creation function. Stopping here to take a break. Created a container and a div element. Also a loop that will create a 16x16 square which is equal to 256. Hence the loop.

---
+++ styles.js
@@ -0,0 +1,12 @@
+/* 16x16 divs */
+function createGrid() {
+    const container = document.createElement('div');
+    container.id = 'main';
+    container.className = 'container';
+    document.body.appendChild(container);
+    const main = document.getElementById(main);
+    for(let i = 0; i < 256; i += 1 ) {
+
+    }
+
+}
